a1c27a50fe1f50e13f0e23be959ad5802ef91b4f
Tushar Pokle <tushar.pokle@gmail.com>
2017-06-13 21:41:56 +1000

    Switched to newtype with show instances

--- src/Main.purs
+++ src/Main.purs
@@ -3,7 +3,8 @@ module Main where
 import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Parse as P
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
-  log "Hello sailor!"
+  log (show (P.string "x" (P.ParseState { inp: "x", out: "x"})))
--- src/Parse.purs
+++ src/Parse.purs
@@ -1,13 +1,25 @@
 module Parse where
 
-import Data.Either
+import Data.Either (Either(..))
+import Prelude (class Show, (<>))
 
-data ParseError = Error String
-type ParseState = {
-  in :: String,
-  out:: String
+data ParseError = OhNo String
+newtype ParseState = ParseState {
+  inp :: String,
+  out :: String
 }
+
 type Parser = ParseState -> Either ParseError ParseState
 
+instance showParseError :: Show ParseError where
+  show (OhNo msg) = "Error: " <> msg
+
+instance showParseState :: Show ParseState where
+  show (ParseState {inp, out}) = "inp: " <> inp <> ", out: " <> out
+
+-- |
+-- | 
+
 string :: String -> Parser
-string str = \state -> Left (Error "No way")
+string str = \state -> Left (OhNo "No way")
+
